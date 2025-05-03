48078b788f168617b32dda356a641d1144a2e60d
Domis382 <naviaeddy@gmail.com>
2025-05-03 18:57:59 -0400

    aux11

--- my-app/src/app/components/navbar/NavbarInicioSesion.tsx
+++ my-app/src/app/components/navbar/NavbarInicioSesion.tsx
@@ -1,24 +1,34 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { useRouter } from 'next/navigation';
 
 import { useUser } from '@/hooks/useUser';
 
 import Link from 'next/link';
-export default function NavbarInicioSesion() {
+export default function NavbarInicioSesion({ onBecomeHost }: { onBecomeHost: () => void }) {
   const [activeBtn, setActiveBtn] = useState(0);
   //Token nombre de usuario
   const user = useUser();
 
+  //Foto de perfil navbar
+  const [profilePhotoUrl, setProfilePhotoUrl] = useState<string | null>(null);
+
   const [isMenuOpen, setIsMenuOpen] = useState(false); // Estado del menú
   const router = useRouter();
+  
+  useEffect(() => {
+    if (user?.foto_perfil) {
+      setProfilePhotoUrl(`http://34.69.214.55:3001${user.foto_perfil}`);
+    } else {
+      setProfilePhotoUrl(null); // Si no hay foto, se usa el icono por defecto
+    }
+  }, [user]);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
     router.push('/'); // 🔥 Te saca al login
   };
-
 
   return (
     <div className="px-6 md:px-20 lg:px-40 py-4  border-b border-[rgba(0,0,0,0.05)] font-[var(--fuente-principal)] bg-[var(--blanco)]">
@@ -60,6 +70,14 @@ export default function NavbarInicioSesion() {
                 {user?.nombre_completo || 'Nombre Usuario'}
             </button>
             <div className="flex items-center justify-center px-3 md:px-4">
+              {profilePhotoUrl ? (
+                <img
+                src={profilePhotoUrl}
+                alt="Foto de perfil"
+                className="w-6 h-6 md:w-8 md:h-8 object-cover rounded-full border border-white"
+                />
+                ) : (
+                
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
@@ -72,11 +90,12 @@ export default function NavbarInicioSesion() {
                 clipRule="evenodd"
                 />
                 </svg>
+              )}
             </div>
             
             {/* Componente menú */}
             {isMenuOpen && (
-              <ProfileMenu onLogout={handleLogout} router={router} />
+              <ProfileMenu onLogout={handleLogout} router={router} onBecomeHost={onBecomeHost} />
             )}
         </div>
       </nav>
@@ -84,7 +103,15 @@ export default function NavbarInicioSesion() {
   );
 }
 
-function ProfileMenu({ onLogout, router }: { onLogout: () => void; router: ReturnType<typeof useRouter> }) {
+function ProfileMenu({
+  onLogout,
+  router,
+  onBecomeHost
+}: {
+  onLogout: () => void;
+  router: ReturnType<typeof useRouter>;
+  onBecomeHost: () => void;
+}) {
   return (
     <div className="absolute right-0 top-full mt-2 w-40 bg-[var(--blanco)] border rounded-lg shadow-lg z-[9999] font-[var(--tamaña-bold)]">
       <button 
@@ -93,11 +120,14 @@ function ProfileMenu({ onLogout, router }: { onLogout: () => void; router: Retur
       >
         <h2 className="hover:text-[var(--blanco)]">Ver perfil</h2>
       </button>
+
       <button 
         className="block w-full text-left px-4 py-2 text-[var(--naranja)] hover:bg-[var(--naranja-46)]"
+        onClick={onBecomeHost}
       >
-        <h2 className="hover:text-[var(--blanco)]">X opción</h2>
+        <h2 className="hover:text-[var(--blanco)]">Quiero ser Host</h2>
       </button>
+
       <button 
         className="block w-full text-left px-4 py-2 text-[var(--naranja)] hover:bg-[var(--naranja-46)] rounded-b-lg"
         onClick={onLogout}
