075d624ced7e057645aa98a4598dda297c783a54
Domis382 <naviaeddy@gmail.com>
2025-05-17 01:11:00 -0400

    aux4

--- my-app/src/app/home/homePage/userPerfil/UserPerfil.tsx
+++ my-app/src/app/home/homePage/userPerfil/UserPerfil.tsx
@@ -25,7 +25,7 @@ export default function UserPerfilPage() {
   useEffect(() => {
     if (user?.foto_perfil) {
       setImagePreviewUrl(user.foto_perfil);
-      console.log('✅ Foto cargada:', `http://localhost:3001${user.foto_perfil}`);
+      console.log('✅ Foto cargada:', `https://redibo-back-wtt.vercel.app${user.foto_perfil}`);
     }
   }, [user]);
   if (!user) return null;
