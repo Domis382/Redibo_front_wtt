76e3b6eb1c30c38dcda8a337f7f6f8d1b051a6ca
Domis382 <naviaeddy@gmail.com>
2025-06-05 12:34:54 -0400

    dsad

--- my-app/src/libs/config.ts
+++ my-app/src/libs/config.ts
@@ -1,4 +1,4 @@
 export const BACK_URL =
   typeof window !== "undefined"
     ? "https://redibo-back-wtt.vercel.app" // cuando se ejecuta en navegador
-    : "http://localhost:3000/api"; // cuando se ejecuta en servidor o desarrollo
+    : "http://localhost:3000"; // cuando se ejecuta en servidor o desarrollo
